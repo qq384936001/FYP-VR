af7fab506d510693ebe650ef0f7d239f4afe4e59
200707985 <80800499+200707985@users.noreply.github.com>
2021-07-21 17:05:05 +0800

    We made a demo version 01!
    Sense : Test01
    can go to second floor, pick the AED the save people
    But UI and distance grab fail, try tomorrow

---
+++ Testing3/Assets/SaveHim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveHim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float fuseTime;
+    void Start()
+    {
+        //Invoke("Explode", fuseTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="AED") {
+            Debug.Log("I save you 2");
+            Score.addSavedPeople(1);
+            Explode();
+        }
+    }
+
+    void Explode() {
+        ParticleSystem exp = GetComponent<ParticleSystem>();
+        exp.Play();
+        
+    }
+}
