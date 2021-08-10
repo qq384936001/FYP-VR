4ac15102194d1fbac6e9fe2526df68caab790954
qq384936001 <77480450+qq384936001@users.noreply.github.com>
2021-08-10 14:10:48 +0800

    Update

---
+++ Assets/SteamVR_Input/ControllerAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ControllerAction : MonoBehaviour
+{
+    public SteamVR_Input_Sources type;
+    public SteamVR_Action_Boolean teleport;
+    public SteamVR_Action_Boolean grab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetTeleportDown())
+        {
+            print("Teleport " + type);
+        }
+
+        if (GetGrab())
+        {
+            print("Grab " + type);
+        }
+    }
+
+    public bool GetTeleportDown()
+    {
+        return teleport.GetStateDown(type);
+    }
+
+    public bool GetGrab()
+    {
+        return grab.GetState(type);
+    }
+
+}
+
