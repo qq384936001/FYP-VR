95574b9f28ba4f5d7040ff66faa8374ff8c48e6d
qq384936001 <77480450+qq384936001@users.noreply.github.com>
2021-08-10 16:29:19 +0800

    Controller Correction
    uninstall the steamVR and install again

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -17,11 +17,27 @@ namespace Valve.VR
     public class SteamVR_Input_ActionSet_default : Valve.VR.SteamVR_ActionSet
     {
         
+        public virtual SteamVR_Action_Boolean InteractUI
+        {
+            get
+            {
+                return SteamVR_Actions.default_InteractUI;
+            }
+        }
+        
         public virtual SteamVR_Action_Boolean Teleport
         {
             get
             {
                 return SteamVR_Actions.default_Teleport;
+            }
+        }
+        
+        public virtual SteamVR_Action_Boolean GrabPinch
+        {
+            get
+            {
+                return SteamVR_Actions.default_GrabPinch;
             }
         }
         
@@ -54,6 +70,14 @@ namespace Valve.VR
             get
             {
                 return SteamVR_Actions.default_SkeletonRightHand;
+            }
+        }
+        
+        public virtual SteamVR_Action_Single Squeeze
+        {
+            get
+            {
+                return SteamVR_Actions.default_Squeeze;
             }
         }
         
